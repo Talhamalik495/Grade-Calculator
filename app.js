e4a9c7d0fa40961b1220bbca8e7b7825788b568a
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-05-17 16:43:03 +0500

    100% local app Work Done

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ if (grade > 100) {
 
 }
 else if (grade > 80 && grade <= 100) {
-    document.write(" Grade your A+")
+    document.write(" Grade your A+ Very Excellent")
 }
 else if (grade > 70 && grade <= 80) {
-    document.write("Grade your A")
+    document.write("Grade your A very Good")
 }
 else if (grade > 60 && grade <= 70) {
-    document.write("Grade your B")
+    document.write("Grade your B Good")
 }
 else if (grade > 50 && grade <= 60) {
-    document.write("Grade yourC")
+    document.write("Grade yourC pass")
 }
 else if (grade > 40 && grade <= 50) {
-    document.write("Grade your D")
+    document.write("Grade your D Work Hard")
 }
 else {
     document.write("Grade yourFail")
